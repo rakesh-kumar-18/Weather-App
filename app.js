e57a8ccb9c5b33cd79cb94fb10a20767857a2ca5
Rakesh-kumar-18 <74340157+rakesh-kumar-18@users.noreply.github.com>
2023-02-25 18:37:11 +0530

    improve the interactivity and add more functionality

--- app.js
+++ app.js
@@ -3,34 +3,55 @@ const https = require("https");
 require("dotenv").config();
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-    const query = req.body.cityName;
+
+    let query = req.body.cityName;
     const apiKey = process.env.API_KEY;
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
     https.get(url, function (response) {
 
-        response.on("data", function (data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<p>The weather is currently " + description + ".</p>");
-            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
-        });
+        const statusCode = response.statusCode;
+
+        if (statusCode === 200) {
+
+            response.on("data", function (data) {
+
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.main.temp;
+                const desc = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+                query = query.toUpperCase();
+
+                res.render('weather', { city: query, temperature: temp, explain: desc, image: imageURL });
+
+                res.end();
+            });
+
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
     });
 });
 
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server is running on port 3000.");
+    console.log("Server is running on port 3000");
 });
